6f9de5da1c0bb91dd9e44c9d76ecf44c1ff1f4f2
Justin Hulme <justinhulme2000@gmail.com>
2025-01-02 18:40:27 -0700

    created a tutorCard widget for displaying information on the tutor. Still needs a lot of work and looks awful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
-from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QSizePolicy, QGridLayout, QVBoxLayout
 from PyQt6.QtCore import Qt
 import sys
 
@@ -77,11 +79,60 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Tutor Center")
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        layout = QGridLayout()
+        central_widget.setLayout(layout)
+
+        for row in range(4):
+            for col in range(3):
+                widget = TutorCard("Frodo", "Images/Frodo.png", "Bioengineering", "Senior", "9:00 - 5:00")
+                layout.addWidget(widget, row, col)
+
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(5)
+
         self.showFullScreen()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key.Key_Escape:
             self.close()
+
+class TutorCard(QWidget):
+    def __init__(self, tutor_name, profile_image_path, major, progress, schedule):
+        super().__init__()
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(5, 5, 5, 5)
+        layout.setSpacing(5)
+        self.setLayout(layout)
+
+        tutor_name_label = QLabel(tutor_name)
+        tutor_name_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        tutor_name_label.setStyleSheet("font-weight: bold; font-size: 16px;")
+
+        profile_image_label = QLabel()
+        pixmap = QPixmap(profile_image_path)
+        profile_image_label.setPixmap(pixmap)
+        profile_image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        major_label = QLabel(f"Major: {major} ({progress})")
+        major_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        major_label.setStyleSheet("font-size: 12px; color:gray")
+
+        schedule_label= QLabel(f"Here from {schedule}")
+        schedule_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        schedule_label.setStyleSheet("font-size: 12px; color:gray")
+
+        layout.addWidget(tutor_name_label)
+        layout.addWidget(profile_image_label)
+        layout.addWidget(major_label)
+        layout.addWidget(schedule_label)
+
+
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.setStyleSheet("background-color: lightblue; border: 5px solid orange")
 
 if __name__ == "__main__":
     app = QApplication([])
