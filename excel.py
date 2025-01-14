8d90625d4fbdb3158bdcc7620c56d0f1f22d7d59
Justin Hulme <justinhulme2000@gmail.com>
2025-01-14 15:01:26 -0700

    Changed it so that it was in a class and added a helper function to get today's schedule

--- excel.py
+++ excel.py
@@ -3,29 +3,64 @@ from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.files.file import File
 import io
 import pandas as pd
+from datetime import datetime
 
-url = os.environ["TUTOR_CENTER_PATH"]
-username = os.environ["TUTOR_CENTER_EMAIL"]
-password = os.environ["TUTOR_CENTER_PASSWORD"]
-relative_url = "/sites/ENGRTCManagementTEST/Shared Documents/General/Tutoring Center Info/Schedule.xlsx"
-#https://usu.sharepoint.com/sites/ENGRTCManagementTEST/Shared%20Documents/General/Tutoring%20Center%20Info/Tutor%20Center%20Teams.xlsx
-ctx_auth = AuthenticationContext(url)
-if ctx_auth.acquire_token_for_user(username, password):
-    ctx = ClientContext(url, ctx_auth)
-    web = ctx.web
-    ctx.load(web)
-    ctx.execute_query()
+class ExcelManager:
+    def __init__(self):
+        self.friday_schedule = None
+        self.thursday_schedule = None
+        self.wednesday_schedule = None
+        self.tuesday_schedule = None
+        self.monday_schedule = None
+        self.last_update = None
 
-    response = File.open_binary(ctx, relative_url)
+    def fetch_schedule(self):
+        url = os.environ["TUTOR_CENTER_PATH"]
+        username = os.environ["TUTOR_CENTER_EMAIL"]
+        password = os.environ["TUTOR_CENTER_PASSWORD"]
+        relative_url = "/sites/ENGRTCManagementTEST/Shared Documents/General/Tutoring Center Info/Schedule.xlsx"
+        # https://usu.sharepoint.com/sites/ENGRTCManagementTEST/Shared%20Documents/General/Tutoring%20Center%20Info/Tutor%20Center%20Teams.xlsx
 
-    # save data to BytesIO stream
-    bytes_file_obj = io.BytesIO()
-    bytes_file_obj.write(response.content)
-    bytes_file_obj.seek(0)  # set file object to start
+        ctx_auth = AuthenticationContext(url)
+        if ctx_auth.acquire_token_for_user(username, password):
+            ctx = ClientContext(url, ctx_auth)
+            web = ctx.web
+            ctx.load(web)
+            ctx.execute_query()
 
-    # read file into pandas dataframe
-    df = pd.read_excel(bytes_file_obj, sheet_name=['Print Schedule', 'Tutor Info'])
+            response = File.open_binary(ctx, relative_url)
 
+            # save data to BytesIO stream
+            bytes_file_obj = io.BytesIO()
+            bytes_file_obj.write(response.content)
+            bytes_file_obj.seek(0)  # set file object to start
 
-else:
-    print("Can't authenticate")
+            # read file into pandas dataframe
+            self.monday_schedule =      pd.read_excel(bytes_file_obj, sheet_name='Print Schedule', usecols='B:Q', skiprows=3, nrows=6).values.tolist()
+            self.tuesday_schedule =     pd.read_excel(bytes_file_obj, sheet_name='Print Schedule', usecols='B:Q', skiprows=12, nrows=6).values.tolist()
+            self.wednesday_schedule =   pd.read_excel(bytes_file_obj, sheet_name='Print Schedule', usecols='B:Q', skiprows=21, nrows=6).values.tolist()
+            self.thursday_schedule =    pd.read_excel(bytes_file_obj, sheet_name='Print Schedule', usecols='B:Q', skiprows=30, nrows=6).values.tolist()
+            self.friday_schedule =      pd.read_excel(bytes_file_obj, sheet_name='Print Schedule', usecols='B:Q', skiprows=39, nrows=6).values.tolist()
+
+            self.last_update = datetime.now().timestamp()
+        else:
+            return False
+
+    def get_today_schedule(self):
+        self.fetch_schedule()
+
+        match datetime.today().weekday():
+            case 0:
+                return self.monday_schedule
+            case 1:
+                return self.tuesday_schedule
+            case 2:
+                return self.wednesday_schedule
+            case 3:
+                return self.thursday_schedule
+            case 4:
+                return self.friday_schedule
+
+if __name__ == "__main__":
+    em = ExcelManager()
+    print(em.get_today_schedule())
