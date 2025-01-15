973fb6fa8ba6f26a46ffafdc5d852f6a37d83242
Justin Hulme <justinhulme2000@gmail.com>
2025-01-15 15:16:17 -0700

    got it so that I could download a folder from the tutor center sharepoint

--- get_pictures.py
+++ get_pictures.py
@@ -1,82 +1,37 @@
-# import re
-# #
-# # import requests
-# #
-# # url = "https://engineering.usu.edu/students/tutoring/tutors"
-# HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.13 (KHTML, like Gecko) Mobile/15E148'}
-# #
-# # response = requests.get(url, headers=HEADERS)
-# #
-# # if response.status_code == 200:
-# #     html_content = response.text
-# #     for i, tag in enumerate(re.findall("<img.*?>", html_content)):
-# #         print(i, tag)
-# #
-# #     print("\n\n\n")
-# #     print(html_content)
-# # else:
-# #     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-#
-# from requests_html import HTMLSession
-#
-# session = HTMLSession()
-# response = session.get("https://engineering.usu.edu/students/tutoring/tutors", headers=HEADERS)
-# response.html.render()
-# response.html.render()
-#
-# html_content = response.html.html
-#
-# print(re.findall("<img.*?>", html_content))
-
 import os
 from office365.sharepoint.client_context import ClientContext
 from office365.runtime.auth.user_credential import UserCredential
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.sharepoint.files.file import File
 
 # SharePoint site and credentials
-site_url = os.environ["TUTOR_CENTER_PATH"]
+url = os.environ["TUTOR_CENTER_PATH"]
 username = os.environ["TUTOR_CENTER_EMAIL"]
 password = os.environ["TUTOR_CENTER_PASSWORD"]
 
 # Relative URL of the folder in SharePoint
-folder_relative_url = "/sites/ENGRTCManagementTEST/Shared%20Documents/Engineering%20Memery"
+folder_url = "/sites/ENGRTCManagementTEST/Shared Documents/Engineering Memery"
 #https://usu.sharepoint.com/sites/ENGRTCManagementTEST/Shared%20Documents/Engineering%20Memery
 
-# Local path to save the files
-local_folder_path = "downloaded_folder"
-os.makedirs(local_folder_path, exist_ok=True)
+ctx_auth = AuthenticationContext(url)
+ctx_auth.acquire_token_for_user(username, password)
+ctx = ClientContext(url, ctx_auth)
 
-# Authenticate and connect
-ctx = ClientContext(site_url).with_credentials(UserCredential(username, password))
+folder = ctx.web.get_folder_by_server_relative_url(folder_url)
+files = folder.files
+ctx.load(files)
+ctx.execute_query()
 
-# Function to download folder contents
-def download_folder(folder_url, local_path):
-    folder = ctx.web.get_folder_by_server_relative_url(folder_url)
-    ctx.load(folder)
-    ctx.execute_query()
+def download_large_file(ctx, file_url, local_path):
+    response = File.open_binary(ctx, file_url)
+    response.raise_for_status()
+    with open(local_path, "wb") as local_file:
+        for chunk in response.iter_content(32 * 1024):
+            local_file.write(chunk)
 
-    # List and download files
-    files = folder.files.execute_query()
-    print("Files:", files)
-    for file in files:
-        try:
-            file_name = file.name
-            local_file_path = os.path.join(local_path, file_name)
-            print(f"Downloading: {file_name}")
-            with open(local_file_path, "wb") as local_file:
-                file.download(local_file).execute_query()
-            print(f"Downloaded: {file_name}")
-        except Exception as e:
-            print(f"Error downloading {file_name}: {e}")
-
-    # List and handle subfolders
-    subfolders = folder.folders.execute_query()
-    print("Subfolders:", subfolders)
-    for subfolder in subfolders:
-        subfolder_name = subfolder.name
-        subfolder_local_path = os.path.join(local_path, subfolder_name)
-        os.makedirs(subfolder_local_path, exist_ok=True)
-        download_folder(subfolder.serverRelativeUrl, subfolder_local_path)
-
-# Start download
-download_folder(folder_relative_url, local_folder_path)
-print(f"Download complete. Files saved to: {local_folder_path}")
+for file in files:
+    file_url = file.serverRelativeUrl
+    file_name = file.properties["Name"]
+    local_path = os.path.join("downloaded_images", file_name)
+    download_large_file(ctx, file_url, local_path)
+    print(f"Downloaded {file_name}")
