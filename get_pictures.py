a2ca00eae43df260f60941e4ffe0f66137f2b6f9
Justin Hulme <justinhulme2000@gmail.com>
2025-01-21 15:54:54 -0700

    changed the folder it downloads to

--- get_pictures.py
+++ get_pictures.py
@@ -1,6 +1,5 @@
 import os
 from office365.sharepoint.client_context import ClientContext
-from office365.runtime.auth.user_credential import UserCredential
 from office365.runtime.auth.authentication_context import AuthenticationContext
 from office365.sharepoint.files.file import File
 
@@ -32,6 +31,6 @@ def download_large_file(ctx, file_url, local_path):
 for file in files:
     file_url = file.serverRelativeUrl
     file_name = file.properties["Name"]
-    local_path = os.path.join("downloaded_images", file_name)
+    local_path = os.path.join("Images", file_name)
     download_large_file(ctx, file_url, local_path)
     print(f"Downloaded {file_name}")
