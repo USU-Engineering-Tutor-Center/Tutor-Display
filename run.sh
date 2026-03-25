07d4e7da2dfcc979f46744ad09a1973744571ab4
Cap-Happy7 <coombsz104@gamil.com>
2026-03-25 11:40:23 -0600

    Fixed the bug that the box path name wasn't in quotes and wasn't being read fully.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd /home/tutorcenter/Tutor-Display 
 git pull 
-rclone sync Box:/Engineering Tutoring Center/Schedule.xlsx ./data
+rclone sync Box:'/Engineering Tutoring Center/Schedule.xlsx' ./data
 rclone sync Box:'/Engineering Tutoring Center/Pictures For Display' ./Images
 /home/tutorcenter/Tutor-Display/venv/bin/python src/main.py > ERRORLOG.txt
 echo -e '\nThe system crashed'
